3f8857839fb50907ddec3f1661c5efcc727a473f
Subho Yadav <subho@gide.ai>
2023-06-25 20:19:04 +0530

    Front end for read -img
    the backend lies in node server branch -read-img

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-
+  const [img, setImg] = useState("");
+  const getData = async () => {
+    try {
+      const response = await axios.get("http://localhost:8089/page");
+      console.log(response);
+      const source = `data:image/jpeg;base64,${response.data.image}`;
+      setImg(source);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <>
-      <h1>Fresh react app</h1>
+      <h1>Hey There!!!</h1>
+      <img src={img} alt="" />
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
