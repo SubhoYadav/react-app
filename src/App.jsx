b5e308b2bec9301218e69840860c4bd9c5873285
Subho Yadav <subho@gide.ai>
2023-07-14 23:15:23 +0530

    Experimenting with promises

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
-
+  const [insights, setInsights] = useState("Generating result insights !");
+  async function startExpt() {
+    const response = await axios.post(
+      "http://localhost:8089/promise/experiment"
+    );
+    console.log(response.data);
+    const message = response.data.name;
+    setName(message);
+  }
+  useEffect(() => {
+    startExpt();
+  }, []);
 
   return (
     <>
-      <h1>Fresh react app</h1>
+      <div classname="form-floating mt-5">
+        <textarea
+          className="form-control"
+          placeholder={insights}
+          id="floatingTextarea2"
+          style={{ height: "100px" }}
+        >
+          {insights}
+        </textarea>
+        <label htmlFor="floatingTextarea2">Comments</label>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
